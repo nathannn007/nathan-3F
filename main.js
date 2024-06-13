e0e8c7f5f861bf2b751be64389f1129da680ece2
nathannn007 <106194240+nathannn007@users.noreply.github.com>
2024-06-13 09:45:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
-
-for(let i = 0 ; i < botoes.length; i++){
+const botoes = document.querySelectorAll(".botao")
+const conteudo = document.querySelectorAll(".aba-conteudo")
+const contador = document.querySelectorAll(".contador")
+for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-     for (let j = 0; j< botoes.length ; j++){
-         botoes[j].classList.remove("ativo")
-         conteudo[j].classList.remove("ativo")
-     }   
-     botoes[i].classList.add("ativo")
-     conteudo[i].classList.add("ativo")
-    }
+      for (let j = 0; j < botoes.length; j++){
+           botoes[j].classList.remove("ativo")
+           conteudo[j].classList.remove("ativo")
+      }
+      botoes[i].classList.add("ativo")
+      conteudo[i].classList.add("ativo")
+  }
 }
-
